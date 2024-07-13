d02f154683ccda2f057c8514eb871b2842556723
itsPrudy <dencecira463@gmail.com>
2024-07-13 18:55:53 +0300

    Final

--- server/server.js
+++ server/server.js
@@ -41,7 +41,7 @@ app.use((err, req, res, next) => {
 });
 
 // Start server
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
